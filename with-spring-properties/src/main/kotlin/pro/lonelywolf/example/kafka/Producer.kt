cf5c43e82e8d1fb7fcc844d0a7016b95efc4e5fe
yhlee <yhlee@dozn.co.kr>
2020-04-02 13:44:15 +0900

    불필요한 import 제거

--- with-spring-properties/src/main/kotlin/pro/lonelywolf/example/kafka/Producer.kt
+++ with-spring-properties/src/main/kotlin/pro/lonelywolf/example/kafka/Producer.kt
@@ -1,16 +1,10 @@
 package pro.lonelywolf.example.kafka
 
-import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.SendResult
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
