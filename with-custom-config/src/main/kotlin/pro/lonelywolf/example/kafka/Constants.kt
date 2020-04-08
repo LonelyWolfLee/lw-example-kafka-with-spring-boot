3ea032847fcd41e493175644cb2d588b78d37fc0
hoan.lee <lonelywolf.lee@gmail.com>
2020-04-08 10:48:15 +0900

    Clean up constants

--- with-custom-config/src/main/kotlin/pro/lonelywolf/example/kafka/Constants.kt
+++ with-custom-config/src/main/kotlin/pro/lonelywolf/example/kafka/Constants.kt
@@ -1,5 +1,5 @@
 package pro.lonelywolf.example.kafka
 
 const val BOOTSTRAP_SERVER_ADDRESS = "localhost:9092"
-const val TOPIC = "myTopic2"
+const val TOPIC = "myTopic"
 const val GROUP_ID = "myGroup"
